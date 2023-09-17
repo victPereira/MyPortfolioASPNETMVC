1edd277b790c879d88aa9ac91b9731b2f43b621f
Victor Pereira <vic.torpereira@hotmail.com>
2023-09-17 21:12:05 +0100

    Adicionar arquivos de projeto.

---
+++ MyPortfolioASPNETMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyPortfolioASPNETMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
